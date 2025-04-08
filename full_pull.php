dd86b52b21de8259a7c86697a165b49c8d69e0aa
Chuck Dolan <cfdolan@gs67-5160771.ndc.nasa.gov>
2025-04-08 13:04:14 -0400

    Added prune_pages.php to clean up after renamed/deleted landing pages

--- full_pull.php
+++ full_pull.php
@@ -12,6 +12,10 @@ $na_set = `curl https://hpde.io/naming_auths.json`;
 
 $list = json_decode ($na_set, JSON_OBJECT_AS_ARRAY);
 $ALL_AUTH_INFO = $list['naming_auths'];
+// Track Deprecated along with the active Naming Authorities for this exercise
+$ALL_AUTH_INFO['Deprecated'] = array ();
+$ALL_AUTH_INFO['Deprecated']['repository'] = 'https://github.com/hpde/Deprecated';
+
 $NAMING_AUTHS = array_keys ($ALL_AUTH_INFO);
 
 foreach ($NAMING_AUTHS as $folder) {
---
+++ prune_pages.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This script removes landing pages for Spase descriptors that have been deleted
+ * or renamed and moved to a new location.
+ * Each landing page is cross-checked against the current descriptor repository.
+ * If there is no corresponding descriptor xml file in the repository, the html, xml
+ * and json files in the landing page folder are deleted.
+ */
+ 
+//define ('PAGES_ROOT', 'pages');
+//define ('REPO_ROOT', 'metadata.src');
+
+$PAGES_ROOT = 'pages';
+$REPO_ROOT = 'metadata.src';
+
+// These are non-descriptor files that must remain
+$skips = array ('index.html', 'metadata.html');
+
+$htmls = `find $PAGES_ROOT -name "*.html"`;
+
+$files = explode ("\n", $htmls);
+
+foreach ($files as $f) {
+  if (strlen ($f) < 1) {
+    continue;
+  }
+
+  // Ignore non-descriptor landing pages
+  $ignore = false;
+  foreach ($skips as $s) {
+    if (strpos ($f, $s) !== false) {
+      $ignore = true;
+    }
+  }
+  if ($ignore) {
+//print "skipping $f\n";
+    continue;
+  }
+
+  // See if this landing page still has a descriptor in the Spase repository
+  $descriptor = str_replace ($PAGES_ROOT, $REPO_ROOT, $f);
+  $descriptor = str_replace ('.html', '.xml', $descriptor);
+//print "$descriptor\n";
+  if (file_exists ($descriptor)) {
+//print "$descriptor exists\n";
+    continue;
+  }
+  $json = str_replace ('.html', '.json', $f);
+  $xml = str_replace ('.html', '.xml', $f);
+  print "deleting $f\n$json\n$xml\n";
+  
+}
+
