85a8c463cc75e84e01af43aec84a258596c824eb
Kurokio <zcboquet@gmail.com>
2026-07-23 16:54:59 +0000

    Update weekly refresh script and package-lock.json

--- generate-all-pages.sh
+++ generate-all-pages.sh
@@ -2,6 +2,7 @@
 # Create pages for files in the metadata source folder. 
 # 
 # Author: Todd King - 2020/06/26
+# Edited by Zach Boquet on 3/10/26
 
 SRC=${1:-metadata.src}
 DEST=${2:-metadata}
@@ -18,3 +19,14 @@ cd ..
 grunt -v copy_op
 grunt -v convert_op
 grunt -v
+
+# regenerate all NASA NumericalData and DisplayData schema.org metadata and override into landing pages
+cd /home/ubuntu/spase-generate-pages
+pwd > /home/ubuntu/tmp/cronjob.log 2>&1
+rm -rf /home/ubuntu/NASA
+cp -r metadata.src/NASA/. /home/ubuntu/NASA/
+rm -rf /home/ubuntu/SMWG
+cp -r metadata.src/SMWG/. /home/ubuntu/SMWG/
+rm -rf SPASE_JSONs
+venv/bin/python updateSchemaOrg_JSONs.py #>> /home/ubuntu/tmp/cronjob.log 2>&1
+venv/bin/python fixSchema_Org_Metadata.py /home/ubuntu/spase-generate-pages/freshSchemaOrgUpdates.txt #>> /home/ubuntu/tmp/cronjob.log 2>&1
